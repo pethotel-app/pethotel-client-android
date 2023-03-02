a779b1b15f87d9ae88f75bb39d4e070fb7cc3a75
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2023-03-02 18:02:47 +0900

    add

--- app/src/main/java/com/hyunsungkr/pethotel/MainActivity.java
+++ app/src/main/java/com/hyunsungkr/pethotel/MainActivity.java
@@ -1,14 +1,26 @@
 package com.hyunsungkr.pethotel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+    public Fragment Home;
+    public Fragment Map;
+    public Fragment Favorite;
+    public Fragment MyInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView = findViewById(R.id.bottomNavigationView);
+
     }
 }
