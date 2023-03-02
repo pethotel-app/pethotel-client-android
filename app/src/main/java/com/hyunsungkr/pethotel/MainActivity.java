8cd1a950ebd8839ed8efa28169d2077b2c62b693
leobaek <120348541+leobaek@users.noreply.github.com>
2023-03-02 16:33:02 +0900

    adada

--- app/src/main/java/com/hyunsungkr/pethotel/MainActivity.java
+++ app/src/main/java/com/hyunsungkr/pethotel/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adada
     }
 }
+// 저장 테스트
