e3afbbeb85f9e33482d9738052ac5d86c8933a8b
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2023-03-13 15:11:29 +0900

    부트페이 키 변경

--- app/src/main/java/com/hyunsungkr/pethotel/config/Config.java
+++ app/src/main/java/com/hyunsungkr/pethotel/config/Config.java
@@ -8,6 +8,6 @@ public class Config {
     public static final String PREFERENCE_NAME = "pethotel_app";
     public static final String ACCESS_TOKEN = "access_token";
 
-    public static String access_key = "63f838513049c8001a365caa";
+    public static String access_key = "640ebbea755e27001969221b";
     public static String secret_key = "LNlp7XqBhQ61wmj9RTRkHJx+WtJwLhHK/cmeHSd+Dk4=";
 }
