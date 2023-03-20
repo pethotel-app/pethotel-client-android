6be78d001d10111edc62bb2de70930d8cbcff229
leobaek <120348541+leobaek@users.noreply.github.com>
2023-03-20 18:07:07 +0900

    ㅁㅇㅁㅇ
    ㅁㅇㅁㅇ

--- app/src/main/java/com/hyunsungkr/pethotel/CancelActivity.java
+++ app/src/main/java/com/hyunsungkr/pethotel/CancelActivity.java
@@ -11,8 +11,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hyunsungkr.pethotel.config.Config;
 import com.hyunsungkr.pethotel.model.Hotel;
@@ -20,6 +22,7 @@ import com.hyunsungkr.pethotel.model.MyReservation;
 
 public class CancelActivity extends AppCompatActivity {
 
+    ImageView imgCancel;
     Spinner spinner;
     TextView txtPrice;
     Button btnCancel;
@@ -38,15 +41,29 @@ public class CancelActivity extends AppCompatActivity {
         txtPrice = findViewById(R.id.txtPrice);
         btnCancel = findViewById(R.id.btnCancel);
         spinner = findViewById(R.id.spinner);
+        imgCancel = findViewById(R.id.imgCancel);
 
+        // 인텐트 받아오기
         MyReservation myReservation = (MyReservation) getIntent().getSerializableExtra("myReservation");
+
+        // 스피너 설정
         String CancelPrice = String.valueOf(myReservation.getPrice());
-        txtPrice.setText(CancelPrice);
+        txtPrice.setText(CancelPrice + "원");
 
         ArrayAdapter yearAdapter = ArrayAdapter.createFromResource(this,
                 R.array.cancel_reasons, android.R.layout.simple_spinner_item);
         spinner.setAdapter(yearAdapter);
 
+
+        // X버튼  클릭 리스너
+        imgCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        // 예약취소 버튼 클릭 리스너
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,7 +72,27 @@ public class CancelActivity extends AppCompatActivity {
         });
 
 
+    }
 
+    public void popUp() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(CancelActivity.this);
+        builder.setMessage("예약을 취소하시겠습니까?")
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        //todo: 예약취소 api 호출
+                        Toast.makeText(CancelActivity.this,"예약이 취소되었습니다.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
 
